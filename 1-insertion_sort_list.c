8db523dfa65d86f8cbd48d12fdf568d0423ac7ac
Sammy <myallio94@gmail.com>
2025-04-10 10:03:16 +0000

    save progress

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - insertion sort linked list
+ * @list: pointer to pointer of list to SORT_H
+ * 
+ * Return: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i, j;
+	
+}
+/* for (int i = 1; i < n; ++i) {
+	int key = arr[i];
+	int j = i - 1;
+
+	while (j >= 0 && arr[j] > key) {
+		arr[j + 1] = arr[j];
+		j = j - 1;
+	}
+	arr[j + 1] = key; */
